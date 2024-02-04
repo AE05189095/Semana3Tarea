7cf9f46aca39ae742f2bf4f17dcfe453c51acf45
Angelo Estrada <05189095@galileo.edu>
2024-02-04 13:47:33 -0600

    first commit

---
+++ Codigo horoscopo.js
@@ -0,0 +1,99 @@
+var mes = prompt ("Ingrese mes de nacimiento")
+var dia = prompt ("Ingrese dia de nacimiento")
+
+if (mes == 1){
+  if (dia >= 1 && dia <=20)
+    console.log ("Capricornio")
+    }   
+if (mes == 1){
+  if (dia >= 21 && dia <=31)
+    console.log ("Acuario")
+    }  
+if (mes == 2){
+  if (dia >= 1 && dia <=19)
+    console.log ("Acuario")
+    }   
+if (mes == 2){
+  if (dia >=20  && dia <=29)
+    console.log ("Piscis")
+    }  
+if (mes == 3){
+  if (dia >= 1 && dia <=20)
+    console.log ("Piscis")
+    }   
+if (mes == 3){
+  if (dia >=21  && dia <=31)
+    console.log ("Aries")
+    } 
+if (mes == 4){
+  if (dia >= 1 && dia <=20)
+    console.log ("Aries")
+    }   
+if (mes == 4){
+  if (dia >=21  && dia <=30)
+    console.log ("Tauro")
+    } 
+if (mes == 5){
+  if (dia >= 1 && dia <=20)
+    console.log ("Tauro")
+    }   
+if (mes == 5){
+  if (dia >=21  && dia <=31)
+    console.log ("Geminis")
+    } 
+if (mes == 6){
+  if (dia >= 1 && dia <=20)
+    console.log ("Geminis")
+    }   
+if (mes == 6){
+  if (dia >=21  && dia <=30)
+    console.log ("Cancer")
+    } 
+if (mes == 7){
+  if (dia >= 1 && dia <=22)
+    console.log ("Cancer")
+    }   
+if (mes == 7){
+  if (dia >=23  && dia <=31)
+    console.log ("Leo")
+    } 
+if (mes == 8){
+  if (dia >= 1 && dia <=23)
+    console.log ("Leo")
+    }   
+if (mes == 8){
+  if (dia >=24  && dia <=30)
+    console.log ("Virgo")
+    } 
+if (mes == 9){
+  if (dia >= 1 && dia <=22)
+    console.log ("Virgo")
+    }   
+if (mes == 9){
+  if (dia >=23  && dia <=30)
+    console.log ("Libra")
+    } 
+if (mes == 10){
+  if (dia >= 1 && dia <=23)
+    console.log ("Libra")
+    }   
+if (mes == 10){
+  if (dia >=24  && dia <=31)
+    console.log ("Escorpio")
+    } 
+if (mes == 11){
+  if (dia >= 1 && dia <=22)
+    console.log ("Escorpio")
+    }   
+if (mes == 11){
+  if (dia >=23  && dia <=30)
+    console.log ("Sagitario")
+    }
+if (mes == 12){
+  if (dia >= 1 && dia <=22)
+    console.log ("Sagitario")
+    }   
+if (mes == 12){
+  if (dia >=22  && dia <=31)
+    console.log ("Capricornio")
+    }
